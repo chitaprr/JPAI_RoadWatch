9673fd6e44648e4c4925ff4c016f109c41fe5458
Victoria Koźmińska <vici.koza@gmail.com>
2026-05-26 21:51:20 +0200

    test

--- backend/src/routes/main.controller.ts
+++ backend/src/routes/main.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { SUCCESS } from "../utils/httpCodeResponses/messages";
 
 export const HelloWorldHandler = async (req: Request, res: Response) => {
-  return SUCCESS(res, "RoadWatch API", {
+  return SUCCESS(res, "RoadWatch API TEST", {
     apiVersion: "v1.0.0",
     ipAddress: req.ip,
   });
