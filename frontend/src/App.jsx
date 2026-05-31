cd1b89d19b38c6a533b7a84e1f4f4b4ee3098a3b
Victoria Koźmińska <vici.koza@gmail.com>
2026-05-31 20:27:50 +0200

    fix lint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'; 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
     // To jest główne pudełko. Dajemy mu 100vh, czyli 100% wysokości ekranu
     <div style={{ height: "100vh", width: "100vw" }}>
-      
       {/* Tu zaczyna się nasza mapa! Ustawiamy środek na Kraków */}
-      <MapContainer center={[50.0647, 19.9450]} zoom={13} style={{ height: "100%", width: "100%" }}>
-        
+      <MapContainer
+        center={[50.0647, 19.945]}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
         {/* To jest warstwa, która pobiera obrazki (kafelki) mapy z internetu */}
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        
-        {/* A to jest pinezka wbita w mapę! */}
-        <Marker position={[50.0647, 19.9450]}>
-          <Popup>
-            JPAI RoadWatch - Nasza pierwsza pinezka!
-          </Popup>
-        </Marker>
 
+        {/* A to jest pinezka wbita w mapę! */}
+        <Marker position={[50.0647, 19.945]}>
+          <Popup>JPAI RoadWatch - Nasza pierwsza pinezka!</Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
